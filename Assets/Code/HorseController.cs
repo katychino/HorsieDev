f505373221bd4f946c2c76b60ef065dcbbf5aedb
katy <nurtest@gmail.com>
2020-08-25 20:36:40 +0200

    all files

---
+++ Assets/Code/HorseController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorseController : MonoBehaviour
+{
+   public float speed = 4; 
+    public float rotSpeed = 80; 
+    public float gravity = 8; 
+
+    Vector3 moveDir = Vector3.zero; // like Vector3 (0,0,0)
+
+    	private CharacterController cc;
+    Animator animator;
+
+    void start(){
+        cc = GetComponent<CharacterController>();
+        animator = GetComponent<Animator> (); //Referenzing 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+     if (cc.isGrounded) {
+            if(Input.GetKey(KeyCode.W)) {
+                 Debug.Log(" w was pressed");
+             //   moveDir = new Vector3(0,0,1); //Only move on Z aksis
+                moveDir *= speed; 
+                 Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            }
+     }
+
+    moveDir.y -= gravity * Time.deltaTime;  
+cc.Move (moveDir * Time.deltaTime);
+    }
+}
---
+++ Assets/PlayerMoveS.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMoveS : MonoBehaviour {
+
+	[Header("Settings")]
+	public float speed = 10f;
+
+	// COMPONENTS
+	private CharacterController cc;
+
+	//============================================
+	// FUNCTIONS (UNITY)
+	//============================================
+
+	void Awake()
+	{
+		cc = GetComponent<CharacterController>();
+	}
+
+	void Update()
+	{	
+		float v = Input.GetAxis("Vertical");
+		float h = Input.GetAxis("Horizontal");
+		
+	
+		Vector3 forward = transform.forward * v * speed * Time.deltaTime;
+		Vector3 right = transform.right * h * speed * Time.deltaTime;
+
+	
+		cc.Move(forward + right);
+	}
+    }
